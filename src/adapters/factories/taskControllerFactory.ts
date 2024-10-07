9b89ebad25a5cddac7d319a2eb7a2a18174acf0b
Tarcisio Ferreira <tarcisiosaf@gmail.com>
2024-10-07 17:16:17 -0300

    feat: storage error logs in mongodb

--- src/adapters/factories/taskControllerFactory.ts
+++ src/adapters/factories/taskControllerFactory.ts
@@ -3,11 +3,13 @@ import { TaskMongoRepository } from "../../dataSources/db/repository/taskMongoRe
 import { LogErrorControllerDecorator } from "../../decorators/logErrorControllerDecorator";
 import { AddTaskController } from "../controllers/task/addTask";
 import { DateValidatorAdapter } from "../dateValidatorAdapter";
+import { LogErrorMongoRepository } from "../../dataSources/db/repository/logErrorMongoRepository";
 
 export const taskControllerFactory = () => {
   const dateValidatorAdapter = new DateValidatorAdapter();
   const taskMongoRepository = new TaskMongoRepository();
   const dbAddTask = new DbAddTask(taskMongoRepository);
   const taskController = new AddTaskController(dbAddTask, dateValidatorAdapter);
-  return new LogErrorControllerDecorator(taskController);
+  const logErrorMongoRepository = new LogErrorMongoRepository();
+  return new LogErrorControllerDecorator(taskController, logErrorMongoRepository);
 };
--- src/decorators/logErrorControllerDecorator.ts
+++ src/decorators/logErrorControllerDecorator.ts
@@ -1,12 +1,16 @@
 import { Controller } from "../adapters/interfaces/controller";
 import { HttpRequest, HttpResponse } from "../adapters/interfaces/http";
+import { LogErrorRepository } from "../usecases/repository/logErrorRepository";
 
 export class LogErrorControllerDecorator implements Controller{
-  constructor(private readonly controller: Controller){}
+  constructor(
+    private readonly controller: Controller,
+    private readonly logErrorRepository: LogErrorRepository
+  ){}
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     const httpResponse = await this.controller.handle(httpRequest);
     if(httpResponse.statusCode === 500){
-      console.error("log do erro");
+      await this.logErrorRepository.log(httpResponse.body.stack);
     }
     return httpResponse;
   }
